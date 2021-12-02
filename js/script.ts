c4659c14b842347c775d16e734a038c7392a8018
Jantar <davidbednarek@email.cz>
2021-12-02 16:03:09 +0100

    Converted all to TypeScript

--- js/script.ts
+++ js/script.ts
@@ -1,20 +1,24 @@
 var num = 0
 var money = 2
 
-const clamp = (num, min, max) => Math.min(Math.max(num, min), max)
-const rnd = (min, max) => Math.round(min + (Math.random() * (max - min)))
-const $ = (id) => document.getElementById(id)
+const clamp = (num: number, min: number, max: number) => Math.min(Math.max(num, min), max)
+const rnd = (min: number, max: number) => Math.round(min + (Math.random() * (max - min)))
+const $ = (id: string) => document.getElementById(id)
 
 var allArray = []
 var jewArray = []
 var blackArray = []
 
 class type {
-  constructor(arr, counter, buyPrice) {
+  arr: Array<person>
+  counter: HTMLElement
+  buyPrice: number
+  sellPrice: number
+  constructor(arr: Array<person>, counter: HTMLElement, buyPrice: number) {
     this.arr = arr
     this.counter = counter
     this.buyPrice = buyPrice
-    this.sellPrice = buyPrice * 1.5
+    this.sellPrice = Math.round(buyPrice * 1.5)
   }
 }
 
@@ -26,7 +30,7 @@ var height = Math.max(document.body.scrollHeight, document.body.offsetHeight,
 var width = Math.max(document.body.scrollWidth, document.body.offsetWidth,
   document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth)
 
-function isCollide(a, b) {
+function isCollide(a: Element, b: Element) {
   var aRect = a.getBoundingClientRect();
   var bRect = b.getBoundingClientRect();
 
@@ -38,46 +42,54 @@ function isCollide(a, b) {
   );
 }
 
-function addMoney(amount) {
+function addMoney(amount: number) {
   money += amount
   $('moneyCounter').innerHTML = Math.round(money) + ' RM'
 }
 
-function buy (type) {
-  if(money >= type.buyPrice) {
-    const p = new person(200, 100, num, type)
+function buy(type: type) {
+  if (money >= type.buyPrice) {
+    const p = new person(200, 100, num.toString(), type)
     addMoney(-type.buyPrice)
     type.arr.push(p)
     allArray.push(p)
+    p.build()
+    type.counter.innerHTML = type.arr.length.toString()
   }
 }
 
-function buyJew () {
+function buyJew() {
   if (money >= 2) {
     addMoney(-2)
-    const p = new person(200, 100, num, typeJew)
+    const p = new person(200, 100, num.toString(), typeJew)
     num++
     jewArray.push(p)
     allArray.push(p)
     p.build()
-    $('jewCounter').innerHTML = jewArray.length
+    $('jewCounter').innerHTML = jewArray.length.toString()
   }
 }
 
 function buyNigger() {
   if (money >= 10) {
     addMoney(-10)
-    const p = new person(200, 100, num, typeBlack)
+    const p = new person(200, 100, num.toString(), typeBlack)
     num++
     blackArray.push(p)
     allArray.push(p)
     p.build()
-    $('blackCounter').innerHTML = blackArray.length
+    $('blackCounter').innerHTML = blackArray.length.toString()
   }
 }
 
 class person {
-  constructor(height, width, id, type) {
+  type: type
+  width: number
+  height: number
+  id: string
+  isDown: boolean
+  addY: number
+  constructor(height: number, width: number, id: string, type: type) {
     this.type = type
     this.width = width
     this.height = height
@@ -90,15 +102,15 @@ class person {
 
     var tag = document.createElement('div')
 
-    tag.style.top = 0
-    tag.style.left = width * 0.5
+    tag.style.top = '0'
+    tag.style.left = (width * 0.5).toString()
 
     document.body.addEventListener('mousemove', event => {
       if (this.isDown) {
         let moveX = event.pageX - (this.width / 2) + event.movementX
         let moveY = event.pageY - (this.height / 2) + event.movementY
-        tag.style.top = clamp(moveY, 0, height - this.height)
-        tag.style.left = clamp(moveX, 0, width * 0.65 - this.width)
+        tag.style.top = clamp(moveY, 0, height - this.height).toString()
+        tag.style.left = clamp(moveX, 0, width * 0.65 - this.width).toString()
       }
     }, true)
 
@@ -107,11 +119,11 @@ class person {
     }, true)
 
     tag.addEventListener('mousedown', event => {
-      let num = document.getElementsByClassName('person')
+      let num = Array.from(document.getElementsByClassName('mat-form-field-infix') as HTMLCollectionOf<HTMLElement>)
       for (var i = 0; i < num.length; i++) {
-        num[i].style.zIndex = 0
+        num[i].style.zIndex = '0'
       }
-      tag.style.zIndex = 1
+      tag.style.zIndex = '1'
       this.isDown = true
     }, true)
 
@@ -123,10 +135,11 @@ class person {
         tag.style.background = "url('img/BlackTexture.png')"
         break
     }
+
     tag.id = this.id
-   tag.className = 'person'
-    tag.style.height = this.height
-    tag.style.width = this.width
+    tag.className = 'person'
+    tag.style.height = this.height.toString()
+    tag.style.width = this.width.toString()
     tag.style.backgroundPosition = 'center'
     tag.style.backgroundSize = 'cover'
     tag.style.position = 'absolute'
@@ -135,18 +148,19 @@ class person {
     document.body.appendChild(tag)
   }
 
-  destroy () {
+  destroy() {
     const arr = this.type.arr
     arr.splice(arr.indexOf(this), 1)
     allArray.splice(arr.indexOf(this), 1)
+    this.type.counter.innerHTML = arr.length.toString()
     document.body.removeChild(document.getElementById(this.id))
   }
 
-  applyGravity () {
+  applyGravity() {
     if (!this.isDown) {
       this.addY += .6
       const topVal = clamp(parseInt($(this.id).style.top, 10), 0, height - this.height)
-      $(this.id).style.top = Math.min((topVal + this.addY), height - this.height)
+      $(this.id).style.top = Math.min((topVal + this.addY), height - this.height).toString()
     } else {
       this.addY = 0
     }
@@ -160,24 +174,7 @@ class person {
   }
 }
 
-setInterval (function() { // LOOP
-
-  /*for (let i = 0; i < jewArray.length; i++) {
-    jewArray[i].applyGravity()
-    if (isCollide($(jewArray[i].id), $('fire'))) {
-      addMoney(5)
-      jewArray[i].destroy()
-    }
-  }
-
-  for (let i = 0; i < blackArray.length; i++) {
-    blackArray[i].applyGravity()
-    if (isCollide($(blackArray[i].id), $('fire'))) {
-      addMoney(15)
-      blackArray[i].destroy()
-    }
-  }*/
-
+setInterval(function () { // LOOP
   for (let i = 0; i < allArray.length; i++) {
     allArray[i].applyGravity()
     allArray[i].checkFire()
